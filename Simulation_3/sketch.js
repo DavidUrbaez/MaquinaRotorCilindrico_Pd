6b1e299d08449cf6ac1330bdd0f83df6331ecf81
DavidUrbaez <durbaezl@unal.edu.co>
2020-10-14 20:02:42 -0500

    add

--- Simulation_3/sketch.js
+++ Simulation_3/sketch.js
@@ -1,62 +1,101 @@
+let img;
 function setup() {
   createCanvas(800, 500);
   let adjustx=200
   let adjusty=25*2
   
-  slider_Ea = createSlider(0, 300, 150);
-  slider_Ea.style('width', '500px');
-  slider_Ea.position(adjustx+20,height-30+adjusty);
   
-  slider_Pd = createSlider(1*100, 3*100, 1.5*100);
-  slider_Pd.style('width', '80px');
-  slider_Pd.position(adjustx+40,height-100+adjusty);
+    
+  slider_Xs = createSlider(100, 300, 240);
+  slider_Xs.style('width', '80px');
+  slider_Xs.position(adjustx+40,height-250+adjusty);
   
-  slider_Va = createSlider(150, 200, 175);
+  slider_Va = createSlider(6000, 10000, 7967);
+  
   slider_Va.style('width', '80px');
   slider_Va.position(adjustx+40,height-180+adjusty);
   
+  
+  slider_Ea = createSlider(8000, 16000,12000);
+  slider_Ea.style('width', '500px');
+  slider_Ea.position(adjustx+20,height-30+adjusty);
+  
+  slider_Pd = createSlider(20000000,45800000*1.8,45800000);
+  slider_Pd.style('width', '80px');
+  slider_Pd.position(adjustx+40,height-100+adjusty);
+
+  
+  img = loadImage('https://raw.githubusercontent.com/DavidUrbaez/MaquinaRotorCilindrico_Pd/gh-pages/Simulation_3/Unal1.PNG');
 }
 
 
 function draw() {
 
   background(7,35,89);
+  image(img, 30, 30, 200*0.8, 100*0.8);
   textFont('Georgia');
+
   textSize(20);
   fill(255);
-  text('Ea',200, height-30);
-  text('Pd',70, height-110);
-  text('Va',70, height-190);
-  
+  // Text
+  text('Xs = ',40, height-270);
+  text('Va = ',40, height-190);
+  text('Pd = ',40, height-110);
+  text('Ea =',170, height-35);
+  //
   textSize(12);
+  
   text('Hecho por: David Urbaez León - 2020',width*3.5/5, height*19/20);
   
   textSize(30);
   text('Diagrama Fasorial',width*1/3, height*1/10);
   textSize(10);
-  text('Generador Sincrónico (Ra=0ohm - Pd cte)',width*0.37, height*0.15);
+  text('Generador Sincrónico (Ra=0 [ohms] - Pd cte)',width*0.36, height*0.15);
   textSize(20);
-  let val = slider_Ea.value()
+  
+  let Ea_mag = slider_Ea.value()
   let Va_mag = slider_Va.value()
-  
-  translate(width /3, height *0.6);
-  
-  
-    
+  let Xs = slider_Xs.value()/100;  
 
   
-  let Pd = 4-slider_Pd.value()/100
-  let v0 = createVector(0, 0);
+  let Pd = slider_Pd.value()
   
-  let Ea = createVector(150+val-Pd*20,-150+80*(Pd/3-1));
-  let Va = createVector(Va_mag, 0);
+  let factor=200/8000
+ 
+  
+  let Ea_x=sqrt(Ea_mag*Ea_mag-(Pd*Xs/(3*Va_mag))*(Pd*Xs/(3*Va_mag)));
+
+  //Text
+  text(nf(Xs,1,2),40+50, height-270);
+  text(nf(Va_mag/7967,1,2),40+50, height-190);
+  text(nf(Pd/45800000,1,2),40+50, height-110);
+  text(nf(Ea_mag/7967,1,2),170+50, height-35);
+  
+  var dis=95;
+  text('p.u.',40+dis, height-270);
+  text('p.u.',40+dis, height-190);
+  text('p.u.',40+dis, height-110);
+  text('p.u.',170+dis, height-35);
+  
+  //
+  
+ 
+  
+  let v0 = createVector(0, 0);
+
+  let Va = createVector(Va_mag*factor, 0);
+ 
+  let Ea = createVector(Ea_x*factor,-factor*Pd*Xs/(3*Va_mag));
+  
   let j = createVector(0, 1);
   
   let VXa = Ea.copy().sub(Va);
   
-  let Ia=Ea.copy().sub(Va).div(Pd*1/0.6).rotate(HALF_PI)
+  let Ia=Ea.copy().sub(Va).div(Xs).rotate(HALF_PI)
 
+  text('Ia = '+str(nf(Ia.mag()/57.87,1,2))+'p.u.',width*2/3, height*0.8);
   
+  translate(width /3, height *0.6);
     
   drawArrow(Va,VXa,150);
   
@@ -86,8 +125,11 @@ function draw() {
   drawingContext.setLineDash([]);
   
 
-  console.log(Pd)
+
 }
+
+
+
 
 
 
